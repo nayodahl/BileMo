ccd2ffd361093f5e1c8eb148672a119ba0e54c6f
nayodahl <anthony.fachaux@gmail.com>
2020-10-05 16:58:32 +0200

    security bundle added + reseller entity modified + migration

--- src/Entity/Reseller.php
+++ src/Entity/Reseller.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use OpenApi\Annotations as OA;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
@@ -18,7 +19,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *      title="Reseller",
  * )
  */
-class Reseller
+class Reseller implements UserInterface
 {
     /**
      * @ORM\Id
@@ -34,21 +35,6 @@ class Reseller
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     * @Groups({"show_resellers"})
-     * @OA\Property(
-     *     description="reseller Username",
-     *     title="Username",
-     * )
-     */
-    private $username;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-    /**
      * @ORM\Column(type="string", length=255, unique=true)
      * @Groups({"show_resellers"})
      * @OA\Property(
@@ -59,10 +45,23 @@ class Reseller
     private $email;
 
     /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
      * @ORM\OneToMany(targetEntity=Customer::class, mappedBy="reseller", orphanRemoval=true)
      * @Groups({"show_resellers"})
      */
     private $customer;
+
+    /////////////////////////////////////////////////////
 
     public function __construct()
     {
@@ -74,21 +73,22 @@ class Reseller
         return $this->id;
     }
 
-    public function getUsername(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->username;
+        return (string) $this->email;
     }
 
-    public function setUsername(string $username): self
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
     {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -98,12 +98,31 @@ class Reseller
         return $this;
     }
 
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail(?string $email): self
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
@@ -139,5 +158,22 @@ class Reseller
         }
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
